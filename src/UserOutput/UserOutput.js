ef59cfe446d5c96967389f43419b2c20ac06d43b
Semion Shulman <shulman.dev@gmail.com>
2020-12-05 12:48:36 +0200

    Pass a username (of your choice) to UserOutput via props and display it there

--- src/UserOutput/UserOutput.js
+++ src/UserOutput/UserOutput.js
@@ -3,7 +3,7 @@ import React from 'react';
 const userOutput = (props) => {
   return (
     <div class="UserOutput">
-      <p>USERNAME</p>
+      <p>{props.userName}</p>
       <p>FREE TEXT</p>
     </div>
   );
